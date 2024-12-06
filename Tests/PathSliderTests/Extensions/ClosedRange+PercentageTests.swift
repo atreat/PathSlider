e9c485e2f3d99e99f9888aa00a56653581043fbb
Austin Emmons <atreatemmons@gmail.com>
2024-12-06 13:33:00 -0500

    Cleanup of Test target

--- Tests/PathSliderTests/Extensions/ClosedRange+PercentageTests.swift
+++ Tests/PathSliderTests/Extensions/ClosedRange+PercentageTests.swift
@@ -18,7 +18,6 @@ struct Test {
         #expect(range.item(for: 0.1) == 1.0)
         #expect(range.item(for: 0.15) == 1.5)
         #expect(range.item(for: 0.5) == 5.0)
-//        #expect(range.item(for: 0.78) == 7.8)
 
         #expect(range.item(for: 1) == 10.0)
     }
